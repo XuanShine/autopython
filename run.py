b9a94cabf611173fc1093bb0dc9c5fc153416795
XuanShine <xuan.ng@hotmail.com>
2020-03-21 23:17:56 +0100

    fix path added to sys.path before importing server

--- run.py
+++ run.py
@@ -1,11 +1,9 @@
 import sys, os
-from server.views import app
-
 
 C = os.path.abspath(os.path.dirname(__file__))
-
 pyaccess_abs_path = os.path.join(C, "PyAccess")
-
 sys.path.append(pyaccess_abs_path)
+
+from server.views import app
 
 app.run(debug=True, host="0.0.0.0", port=5000, use_reloader=False)
