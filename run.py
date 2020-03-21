e36d036015b4e58646b84eb142036e083a4f067e
XuanShine <xuan.ng@hotmail.com>
2020-03-21 22:49:03 +0100

    logging

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 import sys, os
 from server.views import app
+import logging
+
 
 
 C = os.path.abspath(os.path.dirname(__file__))
+
+logging.basicConfig(filename=os.path.join(C, "run_server.log"), level=logging.DEBUG, format="%(asctime)s -- %(name)s -- %(levelname)s -- %(message)s")
 
 pyaccess_abs_path = os.path.join(C, "PyAccess")
 
