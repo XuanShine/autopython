93d88ce659bda4e660c812889b11457fd155854c
XuanShine <xuan.ng@hotmail.com>
2020-09-08 22:09:41 +0200

    schedule back in init

--- server/pyaccess_server/__init__.py
+++ server/pyaccess_server/__init__.py
@@ -1,11 +1,11 @@
 # from flask import Blueprint
-# import schedule
+import schedule
 
 # pyaccess = Blueprint("pyaccess", __name__, url_prefix="/pyaccess")
 
 from .views import pyaccess
 
-# from PyAccess import lock_door
+from PyAccess import lock_door
 
 # pin_door = 5  # channel 1  serrure porte d’entrée
 # pin_2 = 6  # ch2  lumières enseignes
@@ -15,3 +15,25 @@ from .views import pyaccess
 # pin_6 = 20  # ch6  lumières banque
 # pin_7 = 21  # ch7  lumieres in-reception
 # pin_8 = 26  # ch8
+
+task_auto = schedule.every().hour.do(lock_door.main, 7, 22).tag("auto_door")
+task_auto.run()
+
+# porte
+schedule.every().day.at(f"07:00").do(lock_door.open_door).tag("open_door")
+schedule.every().day.at(f"22:00").do(lock_door.close_door).tag("close_door")
+# lumière enseignes
+schedule.every().day.at(f"06:00").do(lock_door.turn, "off", lock_door.relai[2]).tag("off")
+schedule.every().day.at(f"19:00").do(lock_door.turn, "on", lock_door.relai[2]).tag("on")
+# lumière banque
+schedule.every().day.at(f"22:00").do(lock_door.turn, "off", lock_door.relai[6]).tag("off")
+schedule.every().day.at(f"15:00").do(lock_door.turn, "on", lock_door.relai[6]).tag("on")
+# lumière out-reception
+schedule.every().day.at(f"23:00").do(lock_door.turn, "off", lock_door.relai[3]).tag("off")
+schedule.every().day.at(f"07:00").do(lock_door.turn, "on", lock_door.relai[3]).tag("on")
+# lumière paliers
+schedule.every().day.at(f"08:00").do(lock_door.turn, "off", lock_door.relai[4]).tag("off")
+schedule.every().day.at(f"18:00").do(lock_door.turn, "on", lock_door.relai[4]).tag("on")
+# ch7  lumieres in-reception
+schedule.every().day.at(f"12:00").do(lock_door.turn, "off", lock_door.relai[7]).tag("off")
+schedule.every().day.at(f"15:00").do(lock_door.turn, "on", lock_door.relai[7]).tag("on")
