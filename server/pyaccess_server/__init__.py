c6fe053ffdf12d4afec89e55580aa532778783c2
XuanShine <xuan.ng@hotmail.com>
2020-09-09 03:06:50 +0200

    fix call to "turn" send relai_ch and not pin

--- server/pyaccess_server/__init__.py
+++ server/pyaccess_server/__init__.py
@@ -17,23 +17,24 @@ from PyAccess import lock_door
 # pin_8 = 26  # ch8
 
 task_auto = schedule.every().hour.do(lock_door.main, 7, 22).tag("auto_door")
-task_auto.run()
 
 # porte
-schedule.every().day.at(f"07:00").do(lock_door.open_door).tag("open_door")
+schedule.every().day.at(f"03:00").do(lock_door.open_door).tag("open_door")
 schedule.every().day.at(f"22:00").do(lock_door.close_door).tag("close_door")
 # lumière enseignes
-schedule.every().day.at(f"06:00").do(lock_door.turn, "off", lock_door.relai[2]).tag("off")
-schedule.every().day.at(f"19:00").do(lock_door.turn, "on", lock_door.relai[2]).tag("on")
+schedule.every().day.at(f"03:00").do(lock_door.turn, "off", 2).tag("off")
+schedule.every().day.at(f"19:00").do(lock_door.turn, "on", 2).tag("on")
 # lumière banque
-schedule.every().day.at(f"22:00").do(lock_door.turn, "off", lock_door.relai[6]).tag("off")
-schedule.every().day.at(f"15:00").do(lock_door.turn, "on", lock_door.relai[6]).tag("on")
+schedule.every().day.at(f"22:00").do(lock_door.turn, "off", 6).tag("off")
+schedule.every().day.at(f"15:00").do(lock_door.turn, "on", 6).tag("on")
 # lumière out-reception
-schedule.every().day.at(f"23:00").do(lock_door.turn, "off", lock_door.relai[3]).tag("off")
-schedule.every().day.at(f"07:00").do(lock_door.turn, "on", lock_door.relai[3]).tag("on")
+schedule.every().day.at(f"23:00").do(lock_door.turn, "off", 3).tag("off")
+schedule.every().day.at(f"07:00").do(lock_door.turn, "on", 3).tag("on")
 # lumière paliers
-schedule.every().day.at(f"08:00").do(lock_door.turn, "off", lock_door.relai[4]).tag("off")
-schedule.every().day.at(f"18:00").do(lock_door.turn, "on", lock_door.relai[4]).tag("on")
+schedule.every().day.at(f"08:00").do(lock_door.turn, "off", 4).tag("off")
+schedule.every().day.at(f"18:00").do(lock_door.turn, "on", 4).tag("on")
 # ch7  lumieres in-reception
-schedule.every().day.at(f"12:00").do(lock_door.turn, "off", lock_door.relai[7]).tag("off")
-schedule.every().day.at(f"15:00").do(lock_door.turn, "on", lock_door.relai[7]).tag("on")
+schedule.every().day.at(f"12:00").do(lock_door.turn, "off", 7).tag("off")
+schedule.every().day.at(f"15:00").do(lock_door.turn, "on", 7).tag("on")
+
+task_auto.run()
