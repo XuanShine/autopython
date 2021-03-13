e8e73a7f28b78391e9117a3e30e89e8f0288560c
XuanShine <xuan.ng@hotmail.com>
2021-03-13 06:40:22 +0100

    Due to the big bordel ! Project is separate

--- server/__init__.py
+++ server/__init__.py
@@ -18,18 +18,9 @@ def create_app(config_filename=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///list_rpi.db"
 
-    return app
-
-    
-##########################
-#### Helper Functions ####
-##########################
-
-def initialize_extensions(app):
-    # Since the application instance is now created, pass it to each Flask
-    # extension instance to bind it to the Flask application instance (app)
     db.init_app(app)
 
+    return app
 
 # schedule
 import schedule
